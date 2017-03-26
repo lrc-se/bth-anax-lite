2a54094233255720bfbf88f2985097fe4a0a7d91
Kalle Brisland <kabc16@student.bth.se>
2017-03-26 20:56:13 +0200

    Added common header and footer

---
+++ views/incl/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+    <p>&copy;2017 <a href="mailto:kabc16@student.bth.se">Kalle Brisland</a></p>
+</footer>
+</body>
+</html>
---
+++ views/incl/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $title ?> | Kalles sida</title>
+    <link href="<?= $app->url->asset('css/style.css') ?>" rel="stylesheet">
+</head>
+<body>
+<header>
+<?php include 'navbar.php'; ?>
+</header>
+<main>
