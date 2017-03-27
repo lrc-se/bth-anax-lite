2fe5ef52ec7ee85683cae75146a3a48f75d1f9c4
Kalle Brisland <kabc16@student.bth.se>
2017-03-27 21:02:44 +0200

    More styles

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,50 +1,50 @@
 (function(win, doc) {
-    
+
     "use strict";
-    
+
     var toggle = doc.getElementById("menu-toggle");
-    
+
     function forEach(sel, func) {
         Array.prototype.forEach.call(doc.querySelectorAll(sel), func);
     }
-    
+
+    function hideMenu(level) {
+        forEach(".navbar .sub", function(el) {
+            if (!level || +el.getAttribute("data-level") >= level) {
+                el.classList.remove("open");
+            }
+        });
+    }
+
     function toggleMenu(e) {
         e.preventDefault();
         e.stopPropagation();
         var item = e.target.parentElement;
         var isOpen = item.classList.contains("open");
         var level = +item.getAttribute("data-level");
-        console.log(level);
-        if(level == 1) {
-            forEach(".navbar .sub", function(el) {
-                el.classList.remove("open");
-            });
+        if (level == 1) {
+            hideMenu();
         } else {
-            forEach(".navbar .sub", function(el) {
-                if(+el.getAttribute("data-level") >= level) {
-                    el.classList.remove("open");
-                }
-            });
+            hideMenu(level);
         }
-        if(!isOpen) {
+        if (!isOpen) {
             item.classList.add("open");
         }
     }
-    
+
     forEach(".navbar .sub > a, .navbar .sub > span", function(el) {
         el.addEventListener("click", toggleMenu);
     });
     doc.addEventListener("click", function() {
-        forEach(".navbar .sub", function(el) {
-            el.classList.remove("open");
-        });
-        if(toggle.classList.contains("open")) {
+        hideMenu();
+        if (toggle.classList.contains("open")) {
             toggle.classList.toggle("open");
         }
     });
     toggle.addEventListener("click", function(e) {
         e.stopPropagation();
         toggle.classList.toggle("open");
+        hideMenu();
     });
-    
+
 })(window, document);
