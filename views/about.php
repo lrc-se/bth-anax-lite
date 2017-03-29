2052ede21d1b215187f4e3546f282ec16339578b
Kalle Brisland <kabc16@student.bth.se>
2017-03-29 19:34:09 +0200

    Updated Start and About pages

--- views/about.php
+++ views/about.php
@@ -1,7 +1,12 @@
         <h1>Om webbplatsen</h1>
         <p>
             Denna webbplats är en del i kursen <strong><a href="https://dbwebb.se/oophp">oophp</a></strong> i distansprogrammet <em>Webbprogrammering</em> 
-            på <a href="http://www.bth.se/">Blekinge Tekniska Högskola</a>. Kursen behandlar främst objektorienterad PHP och databaskopplingar.
+            på <a href="http://www.bth.se/">Blekinge Tekniska Högskola</a>. Kursen behandlar främst objektorienterad PHP och databaskopplingar med MySQL.
+        </p>
+        <p>
+            <a href="http://www.php.net/"><img src="<?= $app->url->asset('img/elephpant.png') ?>" alt="PHP"></a>
+            <span>&nbsp; &nbsp; &nbsp;</span>
+            <a href="https://www.mysql.com/"><img src="<?= $app->url->asset('img/mysql.png') ?>" alt="MySQL"></a>
         </p>
         <h4>Länkar</h4>
         <ul>
--- views/index.php
+++ views/index.php
@@ -1,14 +1,14 @@
         <h1>Kalles sida</h1>
         <p>
-            Goddagens! Jag heter alltså Kalle och kommer från Östersund. Jag föddes en vacker vinterdag för <script>document.write(new Date().getFullYear() - 1983);</script> år sedan här i 
+            Goddagens! Jag heter alltså Kalle och kommer från Östersund. Jag föddes en vacker vinterdag för <?= date('Y') - 1983 ?> år sedan här i 
             <a href="http://www.republikenjamtland.se">Republiken Jamtland</a> – eller ja, om den nu var så vacker vet jag ju faktiskt inte – 
             och har allt sedan dess hedrat världen med min närvaro.
         </p>
         <p>
             Jag har programmerat i en eller annan form sedan jag upptäckte BASIC under 90-talets första hälft och stötte på HTML strax därefter. Det var tider det! 
             Ett antal olika språk och tekniker har följt sedan dess, men jag har alltid haft en förkärlek för webben och gör nu i och med 
-            <a href="https://www.bth.se/distansutbildningar/webbprogrammering">BTH:s distansprogram</a> ett allvarligt försök att utveckla intresset till en mer yrkesmässig roll 
-            (vilket för övrigt redan lyckats!).
+            <a href="https://www.bth.se/distansutbildningar/webbprogrammering">BTH:s distansprogram</a> ett allvarligt försök att utveckla intresset till en mer yrkesmässig roll –
+            vilket för övrigt redan lyckats!
         </p>
         <p><strong>Varför jag är här:</strong></p>
         <ul>
@@ -22,4 +22,7 @@
         <p>
             På fritiden blir det en hel del fjäll och friluftsliv, såväl sommar som vinter, liksom musicerande av olika slag – 
             jag behärskar mer än en typ av tangenter. Och passa er för mina ordvitsar!
+        </p>
+        <p>
+            <img class="img" src="<?= $app->url->asset('image/me.jpg?w=800') ?>" alt="KB">
         </p>
