3084ba99d9527f16e768156da8b7275bd5056695
Kalle Brisland <kabc16@student.bth.se>
2017-04-30 13:19:14 +0200

    Bugfix

--- config/routes/webshop.php
+++ config/routes/webshop.php
@@ -256,5 +256,6 @@ $app->router->post('user/webshop-admin/product/restock/{id}', function ($id) use
  * Stock alerts as JSON.
  */
 $app->router->add('user/webshop-admin/product/alert', function () use ($app) {
+    $app->verifyAdmin();
     $app->response->sendJSON($app->db->query('SELECT * FROM oophp_viewalert;'));
 });
