9986d93028c4148815afa3083b750ba9ae0f5a60
Kalle Brisland <kabc16@student.bth.se>
2017-05-02 20:54:18 +0200

    Added unit tests for calendar Month class

---
+++ test/Calendar/MonthTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LRC\Calendar;
+
+/**
+ * Test cases for calendar class Month.
+ */
+class MonthTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test object instantiation.
+     */
+    public function testInstantiation()
+    {
+        $month = new Month(2017, 2);
+        $this->assertEquals(2017, $month->getYear());
+        $this->assertEquals(2, $month->getNumber());
+        $this->assertEquals(28, $month->getLength());
+    
+        $month = new Month(2016, 2);
+        $this->assertEquals(29, $month->getLength());
+        $this->assertEquals('Februari', $month->getName());
+        
+        $exception = null;
+        $msg = '';
+        try {
+            $month = new Month('wrong', -3);
+        } catch (\Exception $exception) {
+            $msg = $exception->getMessage();
+        }
+        $this->assertInstanceOf(\Exception::class, $exception);
+        $this->assertEquals('Invalid month: wrong--3', $msg);
+    }
+    
+    /**
+     * Test non-trivial methods.
+     *
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function testMethods()
+    {
+        $month = new Month(1999, 12);
+        $firstDate = \DateTime::createFromFormat('Y-m-d', '1999-12-01');
+        $this->assertEquals($firstDate, $month->getFirstDate());
+        $this->assertFalse($month->isToday(31));
+        
+        $today = new \DateTime();
+        $month = new Month($today->format('Y'), $today->format('n'));
+        $this->assertTrue($month->isToday($today->format('j')));
+    }
+}
